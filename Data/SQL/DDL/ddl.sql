3e8e0101ec4a4fa94664a3df3a92ff2d8be27ca3
Arindam Das Biswas <arinmac26@gmail.com>
2026-09-05 17:16:45 +0530

    Create database and table for black Friday analysis

--- Data/SQL/DDL/ddl.sql
+++ Data/SQL/DDL/ddl.sql
@@ -1,1 +1,33 @@
+CREATE DATABASE IF NOT EXISTS black_friday_analysis;
 
+USE black_friday_analysis;
+
+CREATE TABLE IF NOT EXISTS black_friday_data
+(
+    User_ID                         INT NOT NULL,
+    Product_ID                      VARCHAR(20) NOT NULL,
+    Gender                          CHAR(1) NOT NULL,
+    Age                             VARCHAR(10) NOT NULL,
+    Occupation                      INT NOT NULL,
+    City_Category                   CHAR(1) NOT NULL,
+    Stay_In_Current_City_Years      VARCHAR(5) NOT NULL,
+    Marital_Status                  TINYINT NOT NULL,
+    Product_Category_1              INT NOT NULL,
+    Product_Category_2              INT NULL,
+    Product_Category_3              INT NULL,
+    Purchase                        INT NOT NULL,
+
+    PRIMARY KEY (User_ID, Product_ID),
+
+    CONSTRAINT chk_purchase_positive
+        CHECK (Purchase > 0),
+
+    CONSTRAINT chk_gender
+        CHECK (Gender IN ('M', 'F')),
+
+    CONSTRAINT chk_city_category
+        CHECK (City_Category IN ('A', 'B', 'C')),
+
+    CONSTRAINT chk_marital_status
+        CHECK (Marital_Status IN (0, 1))
+);
